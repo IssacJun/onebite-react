af9ed04ab49a3d769aaeb51ab8afe30edc489596
IssacJun <imwy0115@gmail.com>
2026-01-23 18:00:49 +0900

    5.6 Managing the State

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,43 +1,33 @@
 import './App.css'
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
-import Button from './components/Button'
+import { useState } from 'react';   // State 쓰려면 import해야
 
 // vite에선 컴포넌트 경로를 안써도 됨
 
 function App() {
-  const buttonProps = {
-    text: "카페",
-    color: "blue",
-    a: 1,
-    b: 2,
-    c: 3
-  };
-
-  // HTML 태그를 반환하는 함수를 컴포넌트라고 함, 이건 App 컴포넌트
-
-  // {...buttonProps} : 스프레드 연산자, 객체 안에 있는 모든 속성값들을 다 props로 한번에 전달함
-
-  // props 안에는 html, js 코드와 html 요소, 리액트 컴포넌트도 전달 가능
-  // 자식 컴포넌트에게 children props로 자동으로 전달됨
+  const [ count, setCount ] = useState(0);
+  const [ light, setLight ] = useState("OFF");
+  // console.log(state); // 배열, [undefined, f] 반환됨
+  // undefined: 새롭게 생성된 state의 값, useState 매개변수로 0 입력하면 0이 출력됨
+  // f: 상태 변화 함수, state 값을 변경시킬 수 있는 함수
+  // 리액트에선 state의 값이 변경됐을 때만 리렌더링 됨
+  
   return (
     <>
-      <Button text={"메일"} color={"red"}/>
-      <Button {... buttonProps}/> 
-      <Button text={"블로그"}>
-        <div>자식요소</div>
-      </Button>
-      <Button>
-        <Header />
-      </Button>
+      <div>
+        <h1>{ light }</h1>
+        <button onClick={() => {
+          setLight( light === "OFF" ? "ON" : "OFF" )
+        }}>{ light === "OFF" ? "켜기" : "끄기"  }</button>
+      </div>
+      <div>
+        <h1>{ count }</h1>
+        <button onClick={() => {
+          setCount(count + 1)
+        }}></button>
+      </div>
     </>
   );
-  // App: 부모 컴포넌트
-  // Header, Main, Footer: 자식 컴포넌트
-  // 모든 리액트 컴포넌트는 화면에 렌더링되기 위해서 App 컴포넌트(루트 컴포넌트)를 최상위 조상으로 가짐
-  // 나머지는 모두 자식 컴포넌트 => 계층 구조 가짐
-  // 관례상 루트 컴포넌트 이름은 App 컴포넌트  
+  // ==> 버튼 클릭했을 때 리렌더링, App 함수를 다시 호출하고 새로운 값 렌더링 -> 리렌더링
 }
 
 export default App
