cdbda587f39ca5f85bcae893149df7a9a76dd425
IssacJun <imwy0115@gmail.com>
2026-01-24 13:29:09 +0900

    5.10 Creating the variables of the component with useRef

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 // 회원가입 폼
 // 이름, 생년월일, 국적, 자기 소개
@@ -11,18 +11,34 @@ const Register = () => {
         bio: ""
     }); // 기본값 설정
 
+    // useRef 사용
+    const countRef = useRef(0); // 초기값 없을 때 console.log(refObj); // {current: undefined}
+    const inputRef = useRef();
+
     // 통합 이벤트 핸들러(스프레드 연산자 사용)
-    const onChange = (e) => {        
+    const onChange = (e) => {      
+        countRef.current ++;
+        console.log(countRef.current);
+        
         setInput({
             ...input,
             [e.target.name]: e.target.value
         }) // [e.target.value] 프로퍼티 키 설정함
     }
 
+    const onSubmit = () => {
+        if (input.name === "") {
+            // console.log(inputRef.current);
+            inputRef.current.focus();
+            // inputRef에 있는 DOM 요소를 focus()
+        }
+    }
+
     return (
         <div>
             <div>
                 <input
+                    ref={inputRef} // 이 Input 태그가 렌더링하는 DOM 요소가 inputRef에 저장됨
                     name="name" 
                     value={input.name} 
                     onChange={onChange} 
@@ -48,6 +64,9 @@ const Register = () => {
             </div>
             <div>
                 <textarea name="bio" value={input.bio} onChange={onChange} />
+            </div>
+            <div>
+                <button onClick={onSubmit}>제출</button>
             </div>
         </div>
     );
