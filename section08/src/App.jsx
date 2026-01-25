8492de8df4062f192e6ad70b4322e3da5268daca
IssacJun <imwy0115@gmail.com>
2026-01-25 22:53:07 +0900

    8.2 Implement UI

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,10 +1,15 @@
+import Header from './components/Header'
+import Editor from './components/Editor'
+import List from './components/List'
 import './App.css'
 
 function App() {
-
   return (
-    <>TodoList
-    </>
+    <div className='App'>
+      <Header />
+      <Editor />
+      <List />
+    </div>
   )
 }
 
