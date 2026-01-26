93809009a1627f80d0d241d62f2f8d0f9b52d6bf
IssacJun <imwy0115@gmail.com>
2026-01-26 21:12:35 +0900

    8.4 Create - Add Todo item

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -2,7 +2,7 @@ import Header from './components/Header'
 import Editor from './components/Editor'
 import List from './components/List'
 
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 import './App.css'
 
@@ -27,12 +27,28 @@ const mockData = [
   ]
 
 function App() {
-  const [ todos, useTodos ] = useState(mockData);
+  const [ todos, setTodos ] = useState(mockData);
+  const idRef = useRef(3);
+  const onCreate = (content) => {
+    const newData = {
+      id: idRef.current ++, // 고유 아이템
+      isDone: false,
+      content,
+      date: new Date().getTime(),
+    }
+
+    // todos.push(newData) 
+    // 이렇게 하면 안됨
+    // todos는 state이기 때문에 useTodos와 같은 상태 변화 함수를 호출해야 함
+    // -> 변경된 값을 리액트가 감지하고 리렌더링을 해줌
+
+    setTodos([newData, ...todos]) // 새로운 값을 앞에, 기존의 값을 펼처서 뒤에 배치
+  }
 
   return (
     <div className='App'>
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   )
