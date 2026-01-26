920324a5f24dfc6df9b36e26d87be1759e5d3a77
IssacJun <imwy0115@gmail.com>
2026-01-26 20:49:44 +0900

    8.3 Implements & create Mock data

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,9 +1,34 @@
 import Header from './components/Header'
 import Editor from './components/Editor'
 import List from './components/List'
+
+import { useState } from 'react'
+
 import './App.css'
 
+// Mock
+const mockData = [
+    {
+      id: 0,
+      isDone: false,
+      content: "React 공부",
+      date: new Date().getTime(),
+    }, {
+      id: 1,
+      isDone: false,
+      content: "빨래",
+      date: new Date().getTime(),
+    }, {
+      id: 2,
+      isDone: false,
+      content: "노래",
+      date: new Date().getTime(),
+    }
+  ]
+
 function App() {
+  const [ todos, useTodos ] = useState(mockData);
+
   return (
     <div className='App'>
       <Header />
