f3b108ec2562d1fa242657fd3de9706f5e60306b
IssacJun <imwy0115@gmail.com>
2026-01-27 21:52:31 +0900

    9.1 Introduce useReducer

---
+++ section09/src/App.jsx
@@ -0,0 +1,94 @@
+import Header from './components/Header'
+import Editor from './components/Editor'
+import List from './components/List'
+import Exam from './components/Exam'
+
+import { useState, useRef } from 'react'
+
+import './App.css'
+
+// Mock
+const mockData = [
+    {
+      id: 0,
+      isDone: false,
+      content: "React 공부",
+      date: new Date().getTime(),
+    }, {
+      id: 1,
+      isDone: false,
+      content: "빨래",
+      date: new Date().getTime(),
+    }, {
+      id: 2,
+      isDone: false,
+      content: "노래",
+      date: new Date().getTime(),
+    }
+  ]
+
+function App() {
+  const [ todos, setTodos ] = useState(mockData);
+  const idRef = useRef(3);
+  const onCreate = (content) => {
+    const newData = {
+      id: idRef.current ++, // 고유 아이템
+      isDone: false,
+      content,
+      date: new Date().getTime(),
+    }
+
+    // todos.push(newData) 
+    // 이렇게 하면 안됨
+    // todos는 state이기 때문에 useTodos와 같은 상태 변화 함수를 호출해야 함
+    // -> 변경된 값을 리액트가 감지하고 리렌더링을 해줌
+
+    setTodos([newData, ...todos]) // 새로운 값을 앞에, 기존의 값을 펼처서 뒤에 배치
+  }
+
+  const onUpdate = (targetId) => {
+    // setTodos(todos.map((todo) => {
+    //   if (todo.id === targetId) {
+    //     return {
+    //       ...todo,
+    //       isDone: !todo.isDone
+    //     }
+    //   } else {
+    //     return todo;
+    //   }
+    // }))
+
+    // 화살표 함수 삭제로 간소화
+    setTodos(
+      todos.map((todo) => 
+        todo.id === targetId 
+          ? { ...todo, isDone: !todo.isDone } 
+          : todo
+      )
+    )
+  }
+
+  const onDelete = (targetId) => {
+    // todos 배열에서 targetId와 일치하는 id를 가진 요소만 삭제하고 새로운 배열 반환
+    setTodos(
+      // targetId와 일치하지 않는 todo만으로 새로운 배열 반환
+      todos.filter((todo) => todo.id !== targetId )
+    )
+  }
+
+  return (
+    <div className='App'>
+      <Exam />
+      {/* <Header />
+      <Editor onCreate={onCreate} />
+      <List 
+        todos={todos} 
+        onUpdate={onUpdate} 
+        onDelete={onDelete}
+      />
+       */}
+    </div>
+  )
+}
+
+export default App
