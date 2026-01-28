6f675483bf49626ef35176a42658d6ab815a3e8b
IssacJun <imwy0115@gmail.com>
2026-01-29 08:15:27 +0900

    10.3 React.memo, Optimize components rendering

--- section10/src/components/Header.jsx
+++ section10/src/components/Header.jsx
@@ -1,3 +1,5 @@
+import { memo } from 'react';
+
 import './Header.css'
 
 const Header = () => {
@@ -9,4 +11,4 @@ const Header = () => {
     )
 }
 
-export default Header;
+export default memo(Header);
--- section10/src/components/TodoItem.jsx
+++ section10/src/components/TodoItem.jsx
@@ -1,3 +1,5 @@
+import { memo } from 'react'
+
 import './TodoItem.css'
 
 const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {    
@@ -18,4 +20,21 @@ const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
     )
 }
 
-export default TodoItem
+// memo는 얕은 비교를 함, props로 렌더링 판단함
+// 새로운 todo에 대해 app 컴포넌트 리렌더링하면 그와 관련된 모든 함수가 새로운 함수로 전달됨
+// memo 적용해도 계속 리렌더링됨
+
+// 2번째 인수: 콜백함수 전달 가능, prev nextprops를 전달해서 함수 반환값에 따라 바뀌었는지 판단함
+// T -> Props 바뀌지 않음 -> 리렌더링 X
+// F -> Props 바뀜 -> 리렌더링 O
+export default memo(TodoItem, (prevProps, nexProps) => {
+    // 4가지 값이 같지 않을 때(값이 바뀌지 않았을 때)
+    // false 반환 -> 리렌더링
+    if (prevProps.id !== nexProps.id) return false;
+    if (prevProps.isDOne !== nexProps.isDOne) return false;
+    if (prevProps.content !== nexProps.content) return false;
+    if (prevProps.date !== nexProps.date) return false;
+
+    // 4가지 값이 바뀌지 않았을 때, true 반환 -> 리렌더링 하지 않음
+    return true;
+});
